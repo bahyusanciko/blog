a2a978548cd461c2425c0c35f506762ad32f690b
Bahyu Sanciko <bahyumtech@gmail.com>
2020-04-01 01:42:10 +0700

    Updates

---
+++ precache-manifest.40a555c42ae2c1925bb06f322817b9be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f13ed930f568eb3f64e254a3c4cf67b",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "7638fb0e3798ff21001e",
+    "url": "/blog/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f86822dbdffa9e0a0f9a",
+    "url": "/blog/static/js/2.d0d96860.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blog/static/js/2.d0d96860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7638fb0e3798ff21001e",
+    "url": "/blog/static/js/main.e250ca7b.chunk.js"
+  },
+  {
+    "revision": "75ed156f7d81793dd2ff",
+    "url": "/blog/static/js/runtime-main.b1c61ad7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blog/static/media/logo.5d5d9eef.svg"
+  }
+]);
