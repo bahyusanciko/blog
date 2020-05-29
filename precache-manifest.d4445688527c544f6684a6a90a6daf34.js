db059528bb826eb98236793bc43aba53bc758618
Bahyu Sanciko <bahyumtech@gmail.com>
2020-05-29 21:58:09 +0700

    Updates

--- precache-manifest.d4445688527c544f6684a6a90a6daf34.js
+++ precache-manifest.d4445688527c544f6684a6a90a6daf34.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "17bd2db859ce1b250d7f6c4fa1b9e166",
+    "revision": "a2fab1d226f18c6112c9fb4ed77a7b27",
     "url": "/blog/index.html"
   },
   {
-    "revision": "0b0bd52ed5c129bc6d08",
+    "revision": "f0a9436a8c02bb09452a",
     "url": "/blog/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "1f310c4443f0c87a8e06",
-    "url": "/blog/static/js/2.9e3baa1f.chunk.js"
+    "revision": "79d0f408dc7fa80e1a3a",
+    "url": "/blog/static/js/2.63fce904.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/blog/static/js/2.9e3baa1f.chunk.js.LICENSE.txt"
+    "url": "/blog/static/js/2.63fce904.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0b0bd52ed5c129bc6d08",
-    "url": "/blog/static/js/main.db31889b.chunk.js"
+    "revision": "f0a9436a8c02bb09452a",
+    "url": "/blog/static/js/main.e393c591.chunk.js"
   },
   {
     "revision": "75ed156f7d81793dd2ff",
